4d0d34323ce81c5522aa40e25a68dd925d79965d
Marianvsf <marianvsf@gmail.com>
2024-09-21 05:46:37 +0000

    3456 commit

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -23,7 +23,7 @@ export default MyCards1;*/
 
 function MyCards({ imag, title, description}) {
   return (
-    <div className="col-md-3 col-sm-6 text-center my-3">
+    <div className="col-md-3 col-sm-6 text-center my-4">
       <Card style={{width: '19rem', border: '1px RGB(220, 220, 220)' }}>
         <Card.Img variant="top" src={imag} />
         <Card.Body style={{border: '1px solid RGB(220, 220, 220)'}}>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -12,11 +12,11 @@ const Home = () => {
 	return (
 	<div>
       <MyNavbar/>
-	  <div className="d-flex flex-wrap align-content-center mx-5">
-            <Welcome id="welcome"/>
-			<div className="container m-0">
-				<div className="row justify-content-center">
-					<div id="TheCards" className="d-flex w-100 flex-wrap">
+	  <div className="container">
+      <div className="row justify-content-center">
+	  <div className="d-flex flex-wrap mx-5 align-content-center">
+            <Welcome id='welcome'/>
+					<div id="TheCards" className="d-flex w-100 justify-content-center flex-wrap">
 						<MyCards 
 						imag="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4ByS9ZYbtouvFYRybSm_6em1Dn_7vlBic-Q&s"
 						title="Card Title"
