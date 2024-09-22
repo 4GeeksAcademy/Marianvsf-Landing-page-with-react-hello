958174b5a710f998217ab7c188f8652719033243
Marianvsf <marianvsf@gmail.com>
2024-09-22 02:38:49 +0000

    last commit

--- src/js/component/welcomeCard.jsx
+++ src/js/component/welcomeCard.jsx
@@ -6,7 +6,7 @@ function Welcome() {
   return (
     <Card style={{marginTop: '90px',
       padding: '10px', 
-      marginLeft: '20gitpx',
+      marginLeft: '20px',
       marginRight: '47px', 
       background: 'RGB(220, 220, 220)', border: '1px solid RGB(220, 220, 220)' }}>
       <Card.Body>
